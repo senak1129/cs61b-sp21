8720d67db650ade73e1c131fd8a5c8ac15f9f6fa
fireworks <1446871373@qq.com>
2025-05-27 20:28:26 +0800

    2

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -5,21 +5,47 @@ import static org.junit.Assert.*;
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
 import byow.TileEngine.Tileset;
-
+import java.util.Scanner;
 import java.util.Random;
 
 /**
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+//    private static final long SEED = 2873123;
+//    private static final Random RANDOM = new Random(SEED);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int width=60;
+        int height=30;
+        TERenderer testw = new TERenderer();
+        testw.initialize(width, height);
+        TETile[][] test=new TETile[width][height];
+        for(int i=0;i<width;i++){
+            for(int j=0;j<height;j++){
+                test[i][j]=Tileset.NOTHING;
+            }
+        }
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int s = sc.nextInt();
+        addHexagon(test,x,y,s,Tileset.WALL);
+        testw.renderFrame(test);
+    }
+//    private static TETile randomTile() {
+//        int tileNum = RANDOM.nextInt(3);
+//        switch (tileNum) {
+//            case 0: return Tileset.WALL;
+//            case 1: return Tileset.FLOWER;
+//            case 2: return Tileset.NOTHING;
+//            default: return Tileset.NOTHING;
+//        }
+//    }
     public static void addHexagon(TETile[][] world, int x, int y, int s, TETile tile){
         if (x <= 1) {
             return;
         }
-        int cnt=0;
-        if(x<s||y<s*2){
-            return;
-        }
+        int cnt = 0;
         for (int i = x; i <= x + s - 1; i++) {
             for (int j = y - cnt; j < y - cnt + (cnt * 2 + s); j++) {
                 world[i][j] = tile;
@@ -27,7 +53,7 @@ public class HexWorld {
             cnt++;
         }
         cnt--;
-        for (int i = x + s; i <= x + (s * 2); i++) {
+        for (int i = x + s; i < x + (s * 2); i++) {
             for (int j = y - cnt; j < y - cnt + (cnt * 2 + s); j++) {
                 world[i][j] = tile;
             }
