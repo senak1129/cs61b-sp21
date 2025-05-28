718089f2e95371030d0f56680df146999b8cd5b6
fireworks <1446871373@qq.com>
2025-05-28 15:31:18 +0800

    hello

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -14,32 +14,30 @@ import java.util.Random;
 public class HexWorld {
     private static final long SEED = 287312;
     private static final Random RANDOM = new Random(SEED);
-    private static  int height = 50;
-    private static  int width = 30;
-//    public HexWorld(int _width, int _height) {
-//        width=_width;
-//        height=_height;
-//    }
+    private static  int width = 50;
+    private static  int height = 30;
+    public HexWorld(int _width, int _height) {
+        width=_width;
+        height=_height;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         TERenderer testw = new TERenderer();
-        testw.initialize(height, width);
-        TETile[][] test=new TETile[height][width];
-        TETile[][] ttest=new TETile[width][height];
-        for(int i=0;i<height;i++){
-            for(int j=0;j<width;j++){
-                test[i][j]=Tileset.NOTHING;
-            }
-        }for(int i=0;i<width;i++){
-            for(int j=0;j<height;j++){
-                ttest[i][j]=Tileset.NOTHING;
-            }
-        }
+        testw.initialize(width, height);
+        TETile[][] world=new TETile[width][height];
+        init(world);
         int x = sc.nextInt();
         int y = sc.nextInt();
         int s = sc.nextInt();
-        addHexagon(test,ttest,x,y,s,randomTile());
-        testw.renderFrame(ttest);
+        addHexagon(world,x,y,s,randomTile());
+        testw.renderFrame(world);
+    }
+    private static void init(TETile[][] world) {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
     }
     private static TETile randomTile() {
         int tileNum = RANDOM.nextInt(3);
@@ -50,48 +48,28 @@ public class HexWorld {
             default: return Tileset.NOTHING;
         }
     }
-    public static void addHexagon(TETile[][] world,TETile[][] tworld, int x, int y, int s, TETile tile){
-        if (x <= 1) {
+    public static void addHexagon(TETile[][] world, int x, int y, int s, TETile tile) {
+        if (s < 2) {
             return;
         }
-        int cnt = 0;
-        for (int i = x; i <= x + s - 1; i++) {
-            for (int j = y - cnt; j < y - cnt + (cnt * 2 + s); j++) {
-                world[i][j] = tile;
+        // 下半部分
+        for (int i = 0; i < s; i++) {
+            int width = s + 2 * i;
+            int st = x - i;
+            for (int j = 0; j < width; j++) {
+                int idx = st + j;
+                int idy = y + i;
+                    world[idx][idy] = tile;
             }
-            cnt++;
         }
-        cnt--;
-        int cnt1=cnt;
-        for (int i = x + s; i < x + (s * 2); i++) {
-            for (int j = y - cnt; j < y - cnt + (cnt * 2 + s); j++) {
-                world[i][j] = tile;
-            }
-            cnt--;
-        }
-        System.out.println(cnt1);
-        int n=x+s*2;
-        int m=y - cnt1 + (cnt1 * 2 + s);
-        // 旋转中心点（六边形的中心）
-        int centerX = x + s;
-        int centerY = y;
-        for (int i = x; i < x + 2 * s; i++) {
-            for (int j = y - cnt1; j < y + cnt1 + s; j++) {
-                if (world[i][j] == tile) {
-                    // 计算相对于中心的坐标
-                    int relX = i - centerX;
-                    int relY = j - centerY;
-                    // 应用90度顺时针旋转公式：(x,y) -> (y,-x)
-                    int rotatedX = relY;
-                    int rotatedY = -relX;
-                    // 转换回绝对坐标
-                    int newX = centerX + rotatedX;
-                    int newY = centerY + rotatedY;
-                    // 确保坐标在 tworld 范围内
-                    if (newX >= 0 && newX < tworld.length && newY >= 0 && newY < tworld[0].length) {
-                        tworld[newX][newY] = tile;
-                    }
-                }
+        // 上半部分
+        for (int i = 0; i < s; i++) {
+            int width = s + 2 * (s - 1 - i);
+            int st = x - (s-1-i);
+            for (int j = 0; j < width; j++) {
+                int idx = st + j;
+                int idy = y + s + i;
+                    world[idx][idy] = tile;
             }
         }
     }
